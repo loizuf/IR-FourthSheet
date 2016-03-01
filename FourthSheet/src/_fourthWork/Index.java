fb63d9d284f2fbf641edc5f8f23ea58e31024bde
loizuf <loizuf@gmx.de>
2016-03-01 17:55:08 +0100

    changed classes to be accessible for tests and some comments

--- FourthSheet/src/_fourthWork/Index.java
+++ FourthSheet/src/_fourthWork/Index.java
@@ -9,14 +9,20 @@ public class Index {
 	ArrayList<Document> collection;
 	ArrayList<Unigramm> unigramms;
 
-	// TASK 1 start
+	// Bearbeiten sie Aufgabe 1.a hier
 	// indexes all the terms so we can use them
 	public Index(ArrayList<Document> collection) {
 		this.collection = collection;
 	}
 	
+	// Bearbeiten sie Aufgabe 1.c hier
 	public ArrayList<Integer> searchUnigramm(String query, int i) {
 		ArrayList<Integer> result = new ArrayList<>();
 		return result;
+	}
+	
+	// Bearbeiten sie Aufgabe 2 hier
+	public float getCollectionProbability(String word){
+		return 0.0f;
 	}
 }
--- FourthSheet/src/_fourthWork/Unigramm.java
+++ FourthSheet/src/_fourthWork/Unigramm.java
@@ -12,21 +12,27 @@ public class Unigramm {
 	public Unigramm(Document doc) {
 		probabilities = new HashMap<>();
 		calculateProbabilities(doc);
+		name = "Model for: " + doc.getName();
 	}
 	
 	public HashMap<String, Float> getAllProbabilities() {
 		return probabilities;
 	}
 
-	protected float getProbability(String word) {
-		return probabilities.get(word);
+	public float getProbability(String word) {
+		if(probabilities.get(word) != null){
+			return probabilities.get(word);
+		}
+		return 0.0f;
 	}
 	
-	protected String getName() {
+	public String getName() {
 		return name;
 	}
 
-	protected  void calculateProbabilities(Document doc) {
+	// Bearbeiten sie Aufgabe 1.b hier
+	// Bearbeiten sie Aufgabe 2 hier
+	private void calculateProbabilities(Document doc) {
 		
 	}
 	
