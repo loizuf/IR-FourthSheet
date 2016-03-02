9ed8facf2060275dba89b2892078e46c0261f757
Soeren <loizuf.sn@gmail.com>
2016-03-02 22:35:42 +0100

    HAAAAAANDS

--- FourthSheet/src/_fourthTest/TestSingleProbabilities.java
+++ FourthSheet/src/_fourthTest/TestSingleProbabilities.java
@@ -17,6 +17,9 @@ public class TestSingleProbabilities {
 	
 	// ArrayList containing unigrams for all documents
 	private static ArrayList<Unigramm> unis;
+	
+	// calculation help variable
+	float precision = 0.0005f;
 
 	public TestSingleProbabilities() throws FileNotFoundException {
 		ArrayList<Document> collection = FileReader.readCollection(TEST_PATH_3);
@@ -26,18 +29,18 @@ public class TestSingleProbabilities {
 			unis.add(new Unigramm(document));
 		}		
 
-		assertTrue("Term one time in document", checkRange(unis.get(0).getProbability("iowa"), 0.0333f));
-		assertTrue("Term two times in document", checkRange(unis.get(0).getProbability("he"), 0.0666f));
-	    assertTrue("Term in multiple documents", checkRange(unis.get(1).getProbability("spock"), 0.0270f));
+		assertTrue("Term one time in document", checkRange(unis.get(0).getProbability("iowa"), 0.033f));
+		assertTrue("Term two times in document", checkRange(unis.get(0).getProbability("he"), 0.066f));
+	    assertTrue("Term in multiple documents", checkRange(unis.get(1).getProbability("spock"), 0.027f));
 	    assertEquals("Term not in document", unis.get(4).getProbability("kaktus"), 0.0f);
 	    assertEquals("Term in ohter documents", unis.get(4).getProbability("enterprise"), 0.0f);
 	}
 
-	// We cant know how the students calcukate the probability but we can check
+	// We cant know how the students calculate the probability but we can check
 	// if they are in a certain range (and we can do that as precise as we want)
 	private boolean checkRange(float value, float bound) {
 		
-		if(value>=bound-0.0005 && value<=bound + 0.0005){
+		if(value>=bound - precision && value<=bound + precision){
 			return true;
 		}
 		return false;
